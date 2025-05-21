143629969f68bc96c650aac77c026db3e2f6fcc8
Scott <thd0427__@naver.com>
2025-05-21 15:41:20 +0900

    check line 추가

--- sdg/src/main/java/com/shopl/sdg/component/control/check/SDGCheck.kt
+++ sdg/src/main/java/com/shopl/sdg/component/control/check/SDGCheck.kt
@@ -12,11 +12,15 @@ import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.shopl.sdg_common.ext.clickable
 import com.shopl.sdg_common.foundation.SDGColor
@@ -68,5 +72,16 @@ fun SDGCheck(
             contentDescription = ""
         )
 
+    }
+}
+
+@Preview
+@Composable
+private fun SDGCheckPreView() {
+    var test by remember {
+        mutableStateOf(false)
+    }
+    SDGCheck(clickPadding = PaddingValues(20.dp), isChecked = test) {
+        test = !test
     }
 }
--- sdg/src/main/java/com/shopl/sdg/component/control/check/SDGCheckLine.kt
+++ sdg/src/main/java/com/shopl/sdg/component/control/check/SDGCheckLine.kt
@@ -1,15 +1,16 @@
-package com.shopl.sdg.component.control.checkbox
+package com.shopl.sdg.component.control.check
 
 import androidx.compose.animation.animateColorAsState
 import androidx.compose.animation.core.FastOutSlowInEasing
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -18,6 +19,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -27,16 +29,45 @@ import com.shopl.sdg_common.foundation.SDGColor
 import com.shopl.sdg_resource.R
 
 @Composable
-fun SDGCheckBox(
-    isChecked: Boolean,
+fun SDGCheckLine(
+    unCheckedStrokeColor: Color = SDGColor.Neutral300,
+    checkedStrokeColor: Color = SDGColor.Red400,
+    unCheckedColor: Color = SDGColor.Neutral300,
+    checkedColor: Color = SDGColor.Red400,
+    unCheckedBackgroundColor: Color = Color.Transparent,
+    checkedBackgroundColor: Color = SDGColor.Primary300,
+    isChecked: Boolean = false,
     clickPadding: PaddingValues = PaddingValues(),
-    onClick: (() -> Unit)? = null,
+    onClick: (() -> Unit)? = null
 ) {
+
+    val strokeColor by animateColorAsState(
+        targetValue = if (isChecked)
+            checkedStrokeColor
+        else
+            unCheckedStrokeColor,
+        animationSpec = tween(
+            durationMillis = 300,
+            easing = FastOutSlowInEasing
+        )
+    )
+
+    val checkColor by animateColorAsState(
+        targetValue = if (isChecked)
+            checkedColor
+        else
+            unCheckedColor,
+        animationSpec = tween(
+            durationMillis = 300,
+            easing = FastOutSlowInEasing
+        )
+    )
+
     val backgroundColor by animateColorAsState(
         targetValue = if (isChecked)
-            SDGColor.Primary300
+            checkedBackgroundColor
         else
-            SDGColor.Neutral200,
+            unCheckedBackgroundColor,
         animationSpec = tween(
             durationMillis = 300,
             easing = FastOutSlowInEasing
@@ -45,22 +76,25 @@ fun SDGCheckBox(
 
     Box(
         modifier = Modifier
-            .wrapContentSize(align = Alignment.Center)
             .padding(clickPadding)
-            .then(
-                if (onClick != null) {
-                    Modifier.clickable(
-                        hasRipple = false,
-                        onClick = { onClick() }
-                    )
-                } else {
-                    Modifier
+            .border(
+                width = 1.dp,
+                color = strokeColor,
+                shape = CircleShape
+            )
+            .clip(
+                shape = CircleShape
+            )
+            .background(
+                color = backgroundColor,
+            )
+            .clickable(
+                hasRipple = false,
+                onClick = {
+                    onClick?.invoke()
                 }
             )
-            .clip(shape = RoundedCornerShape(4.dp))
-            .background(color = backgroundColor)
     ) {
-
         Image(
             modifier = Modifier
                 .padding(1.dp)
@@ -68,20 +102,19 @@ fun SDGCheckBox(
             painter = painterResource(
                 id = R.drawable.ic_common_check_s,
             ),
-            colorFilter = ColorFilter.tint(SDGColor.Neutral0),
+            colorFilter = ColorFilter.tint(color = checkColor),
             contentDescription = ""
         )
-
     }
 }
 
 @Preview
 @Composable
-private fun SDGCheckBoxPreView() {
+private fun SDGCheckLinePreView() {
     var test by remember {
         mutableStateOf(false)
     }
-    SDGCheckBox(clickPadding = PaddingValues(20.dp), isChecked = test) {
+    SDGCheckLine(clickPadding = PaddingValues(20.dp), isChecked = test) {
         test = !test
     }
 }
