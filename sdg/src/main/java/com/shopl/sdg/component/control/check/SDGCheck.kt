936d2deb0cabeb7d38921afa34ea4532f17934da
Scott <thd0427__@naver.com>
2025-05-21 15:39:16 +0900

    check 추가

---
+++ sdg/src/main/java/com/shopl/sdg/component/control/check/SDGCheck.kt
@@ -0,0 +1,72 @@
+package com.shopl.sdg.component.control.check
+
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.core.FastOutSlowInEasing
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.shopl.sdg_common.ext.clickable
+import com.shopl.sdg_common.foundation.SDGColor
+import com.shopl.sdg_resource.R
+
+@Composable
+fun SDGCheck(
+    isChecked: Boolean,
+    clickPadding: PaddingValues = PaddingValues(),
+    onClick: (() -> Unit)? = null,
+) {
+    val backgroundColor by animateColorAsState(
+        targetValue = if (isChecked)
+            SDGColor.Primary300
+        else
+            SDGColor.Neutral200,
+        animationSpec = tween(
+            durationMillis = 300,
+            easing = FastOutSlowInEasing
+        )
+    )
+
+    Box(
+        modifier = Modifier
+            .wrapContentSize(align = Alignment.Center)
+            .padding(clickPadding)
+            .then(
+                if (onClick != null) {
+                    Modifier.clickable(
+                        hasRipple = false,
+                        onClick = { onClick() }
+                    )
+                } else {
+                    Modifier
+                }
+            )
+            .clip(shape = CircleShape)
+            .background(backgroundColor),
+    ) {
+
+        Image(
+            modifier = Modifier
+                .padding(1.dp)
+                .wrapContentSize(align = Alignment.Center),
+            painter = painterResource(
+                id = R.drawable.ic_common_check_s,
+            ),
+            colorFilter = ColorFilter.tint(SDGColor.Neutral0),
+            contentDescription = ""
+        )
+
+    }
+}
