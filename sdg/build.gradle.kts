b5a0cafe7513778827283aad6ef7be0a861f2a4a
Scott <thd0427__@naver.com>
2025-05-22 14:10:52 +0900

    segment 추가

--- sdg/build.gradle.kts
+++ sdg/build.gradle.kts
@@ -45,6 +45,8 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
 
+    implementation(libs.kotlinx.immutable)
+
     implementation(libs.joda)
 
     testImplementation(libs.junit)
---
+++ sdg/src/main/java/com/shopl/sdg/component/segment/SDGSegment.kt
@@ -0,0 +1,270 @@
+package com.shopl.sdg.component.segment
+
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateDp
+import androidx.compose.animation.core.spring
+import androidx.compose.animation.core.updateTransition
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.sizeIn
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.TabPosition
+import androidx.compose.material3.TabRow
+import androidx.compose.material3.TabRowDefaults.tabIndicatorOffset
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.zIndex
+import com.shopl.sdg_common.ext.clickable
+import com.shopl.sdg_common.ext.dropShadow
+import com.shopl.sdg_common.foundation.SDGColor
+import com.shopl.sdg_common.foundation.typography.SDGTypography
+import com.shopl.sdg_common.ui.components.SDGText
+import kotlinx.collections.immutable.PersistentList
+import kotlinx.collections.immutable.persistentListOf
+
+/**
+ * DesignSystem - Segment
+ * 2~3개의 항목 중 필수로 1개를 선택하는 컴포넌트
+ *
+ * @param type [SDGSegmentType] – 타입에 따라 라벨의 개수가 결정
+ * @param line [SDGSegmentTextLine] – 라벨의 텍스트 레이아웃 스타일을 지정
+ * @param labels 표시할 라벨 문자열 리스트
+ * @param selectedIndex – 현재 선택된 라벨의 인덱스
+ * @param backgroundColor – 세그먼트 컨트롤의 배경 색상
+ * @param onLabelClick 라벨 클릭 시 호출되는 콜백
+ */
+@Composable
+fun SDGSegment(
+    type: SDGSegmentType,
+    line: SDGSegmentTextLine,
+    labels: PersistentList<String>,
+    selectedIndex: Int,
+    backgroundColor: Color,
+    onLabelClick: (Int) -> Unit,
+) {
+    require(labels.size == type.requiredLabelCount) {
+        "라벨 개수를 Type과 맞춰주세요. (라벨 필요 개수: ${type.requiredLabelCount})"
+    }
+    require(selectedIndex in 0..labels.lastIndex) {
+        "라벨 개수에 맞는 인덱스를 입력하세요. (라벨 개수: ${labels.size} / 선택한 인덱스: $selectedIndex) "
+    }
+
+    Box(
+        modifier = Modifier.clip(RoundedCornerShape(12.dp))
+    ) {
+        TabRow(
+            modifier = Modifier.clip(RoundedCornerShape(8.dp)),
+            selectedTabIndex = selectedIndex,
+            containerColor = backgroundColor,
+            indicator = {
+                SDGSegmentIndicator(
+                    tabPositions = it,
+                    selectedIndex = selectedIndex
+                )
+            },
+            divider = {}
+        ) {
+            labels.forEachIndexed { index, label ->
+                SDGSegmentUnit(
+                    line = line,
+                    label = label,
+                    selected = selectedIndex == index,
+                    onLabelClick = {
+                        onLabelClick(index)
+                    }
+                )
+            }
+        }
+    }
+}
+
+@Composable
+private fun SDGSegmentUnit(
+    line: SDGSegmentTextLine,
+    label: String,
+    selected: Boolean,
+    onLabelClick: () -> Unit
+) {
+    when (line) {
+        SDGSegmentTextLine.TWO_LINE -> {
+            SDGTwoLineSegmentUnit(
+                label = label,
+                selected = selected,
+                onLabelClick = onLabelClick
+            )
+        }
+
+        SDGSegmentTextLine.ONE_LINE -> {
+            SDGOneLineSegmentUnit(
+                label = label,
+                selected = selected,
+                onLabelClick = onLabelClick
+            )
+        }
+    }
+}
+
+@Composable
+private fun SDGTwoLineSegmentUnit(
+    modifier: Modifier = Modifier,
+    label: String,
+    selected: Boolean,
+    onLabelClick: () -> Unit
+) {
+    Box(
+        modifier = modifier
+            .zIndex(1f)
+            .sizeIn(minWidth = 50.dp, minHeight = 50.dp)
+            .clip(RoundedCornerShape(8.dp))
+            .clickable(hasRipple = false) { onLabelClick() }
+            .background(Color.Transparent),
+        contentAlignment = Alignment.Center
+    ) {
+        SDGText(
+            modifier = Modifier.padding(vertical = 11.dp, horizontal = 12.dp),
+            text = label,
+            typography = SDGTypography.Body2R,
+            maxLines = 2,
+            overflow = TextOverflow.Ellipsis,
+            textColor = if (selected) SDGColor.Neutral700 else SDGColor.Neutral500
+        )
+    }
+}
+
+@Composable
+private fun SDGOneLineSegmentUnit(
+    modifier: Modifier = Modifier,
+    label: String,
+    selected: Boolean,
+    onLabelClick: () -> Unit
+) {
+    Box(
+        modifier = modifier
+            .zIndex(1f)
+            .sizeIn(minWidth = 32.dp, minHeight = 32.dp)
+            .clip(RoundedCornerShape(8.dp))
+            .background(Color.Transparent)
+            .clickable(hasRipple = false) { onLabelClick() },
+        contentAlignment = Alignment.Center
+    ) {
+        SDGText(
+            modifier = Modifier.padding(vertical = 11.dp, horizontal = 12.dp),
+            text = label,
+            typography = SDGTypography.Body2R,
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis,
+            textColor = if (selected) SDGColor.Neutral700 else SDGColor.Neutral500
+        )
+    }
+}
+
+@Composable
+private fun SDGSegmentIndicator(
+    tabPositions: List<TabPosition>,
+    selectedIndex: Int,
+) {
+    val transition = updateTransition(targetState = selectedIndex, label = "indicator")
+    val indicatorLeft by transition.animateDp(
+        label = "",
+        transitionSpec = {
+            spring(stiffness = Spring.StiffnessLow)
+        }
+    ) {
+        tabPositions[it].left
+    }
+    val indicatorRight by transition.animateDp(
+        label = "",
+        transitionSpec = { spring(stiffness = Spring.StiffnessLow) }
+    ) {
+        tabPositions[it].right
+    }
+
+    Box(
+        Modifier
+            .tabIndicatorOffset(tabPositions[selectedIndex])
+            .padding(4.dp)
+            .dropShadow(
+                shape = RoundedCornerShape(8.dp),
+                offsetX = 1.dp,
+                offsetY = 1.dp,
+                blur = 4.dp,
+                color = SDGColor.Neutral900_a5
+            )
+            .width(indicatorRight - indicatorLeft)
+            .fillMaxHeight()
+            .clip(RoundedCornerShape(8.dp))
+            .background(SDGColor.Neutral0)
+    )
+}
+
+enum class SDGSegmentType(val requiredLabelCount: Int) {
+    THREE_OPTSDGN(requiredLabelCount = 3),
+    TWO_OPTSDGN(requiredLabelCount = 2)
+}
+
+enum class SDGSegmentTextLine {
+    TWO_LINE,
+    ONE_LINE
+}
+
+@Preview(name = "3option + 2line")
+@Composable
+private fun PrevSDGSegment_ThreeOption_TwoLine() {
+    SDGSegment(
+        type = SDGSegmentType.THREE_OPTSDGN,
+        labels = persistentListOf("긴라벨은 말줄임 긴라벨은 말줄임", "짧은라벨2", "짧은라벨3"),
+        line = SDGSegmentTextLine.TWO_LINE,
+        selectedIndex = 1,
+        backgroundColor = SDGColor.Neutral300,
+        onLabelClick = {}
+    )
+}
+
+@Preview(name = "3option + 1line")
+@Composable
+private fun PrevSDGSegment_ThreeOption_OneLine() {
+    SDGSegment(
+        type = SDGSegmentType.THREE_OPTSDGN,
+        labels = persistentListOf("긴라벨은 말줄임 긴라벨은 말줄임", "짧은라벨2", "짧은라벨3"),
+        line = SDGSegmentTextLine.ONE_LINE,
+        selectedIndex = 0,
+        backgroundColor = SDGColor.Neutral100,
+        onLabelClick = {}
+    )
+}
+
+@Preview(name = "2option + 2line")
+@Composable
+private fun PrevSDGSegment_TwoOption_TwoLine() {
+    SDGSegment(
+        type = SDGSegmentType.TWO_OPTSDGN,
+        labels = persistentListOf("1", "라벨2"),
+        line = SDGSegmentTextLine.TWO_LINE,
+        selectedIndex = 0,
+        backgroundColor = SDGColor.Neutral300,
+        onLabelClick = {}
+    )
+}
+
+@Preview(name = "2option + 1line")
+@Composable
+private fun PrevSDGSegment_TwoOption_OneLine() {
+    SDGSegment(
+        type = SDGSegmentType.TWO_OPTSDGN,
+        labels = persistentListOf("라벨1", "라벨2"),
+        line = SDGSegmentTextLine.ONE_LINE,
+        selectedIndex = 1,
+        backgroundColor = SDGColor.Neutral300,
+        onLabelClick = {}
+    )
+}
