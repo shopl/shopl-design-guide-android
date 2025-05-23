df4b0848a642432a5d8157148232b8c3419c92b6
Scott <thd0427__@naver.com>
2025-05-23 13:36:26 +0900

    maven plugin 추가

--- sdg/build.gradle.kts
+++ sdg/build.gradle.kts
@@ -1,9 +1,12 @@
+import com.vanniktech.maven.publish.SonatypeHost
+
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.ksp)
     alias(libs.plugins.hilt)
+    alias(libs.plugins.vanniktech.maven)
 }
 
 android {
@@ -61,4 +64,39 @@ dependencies {
     androidTestImplementation(libs.androidx.espresso.core)
 
     debugImplementation(libs.androidx.ui.tooling)
+}
+
+mavenPublishing {
+    publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
+
+    signAllPublications()
+
+    coordinates("io.github.shopl-dev", "SDG-Android", "0.0.0")
+
+    pom {
+        name = "Shopl-Design-Guide"
+        description = "SDG is a collection of design resources, reusable components and guidelines for creating Shopl's products."
+        url = "https://github.com/shopl/shopl-design-guide-android"
+        inceptionYear = "2025"
+
+        licenses {
+            license {
+                name = "The Apache License, Version 2.0"
+                url = "http://www.apache.org/licenses/LICENSE-2.0.txt"
+            }
+        }
+        developers {
+            developer {
+                id = "shopl-dev"
+                name = "shopl-dev"
+                url = "https://github.com/shopl-dev"
+            }
+        }
+
+        scm {
+            url.set("https://github.com/shopl/shopl-design-guide-android")
+            connection.set("scm:git:git://github.com/shopl/shopl-design-guide-android.git")
+            developerConnection.set("scm:git:ssh://git@github.com:shopl/shopl-design-guide-android.git")
+        }
+    }
 }
