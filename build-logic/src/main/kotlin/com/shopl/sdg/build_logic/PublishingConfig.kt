1e9dda377d937468f90a37b43eb4a602ebccc6ab
Scott <thd0427__@naver.com>
2025-05-27 15:26:04 +0900

    임시 버전 세팅

--- build-logic/src/main/kotlin/com/shopl/sdg/build_logic/PublishingConfig.kt
+++ build-logic/src/main/kotlin/com/shopl/sdg/build_logic/PublishingConfig.kt
@@ -2,7 +2,7 @@ package com.shopl.sdg.build_logic
 
 object PublishingConfig {
     const val GROUP = "io.github.shopl-dev"
-    const val VERSION = "0.0.32"
+    const val VERSION = "0.0.2"
 
     const val SDG_ARTIFACT_ID = "SDG-Android"
     const val SDG_COMMON_ARTIFACT_ID = "SDG-Android-Common"
