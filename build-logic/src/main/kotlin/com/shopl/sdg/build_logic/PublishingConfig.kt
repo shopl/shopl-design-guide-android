3198351e040c00b40458935e7adde3aab71b9841
Scott <thd0427__@naver.com>
2025-05-28 14:27:17 +0900

    회사 도메인 namespace 적용

--- build-logic/src/main/kotlin/com/shopl/sdg/build_logic/PublishingConfig.kt
+++ build-logic/src/main/kotlin/com/shopl/sdg/build_logic/PublishingConfig.kt
@@ -1,8 +1,8 @@
 package com.shopl.sdg.build_logic
 
 object PublishingConfig {
-    const val GROUP = "io.github.shopl-dev"
-    const val VERSION = "0.0.2"
+    const val GROUP = "com.shoplworks"
+    const val VERSION = "0.0.0"
 
     const val SDG_ARTIFACT_ID = "SDG-Android"
     const val SDG_COMMON_ARTIFACT_ID = "SDG-Android-Common"
