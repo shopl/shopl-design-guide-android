cd0c282de68b579864289800f83467f8ef7118e4
Scott <thd0427__@naver.com>
2025-07-08 18:04:13 +0900

    Spacing 재정의

---
+++ sdg-common/src/main/java/com/shopl/sdg_common/foundation/spacing/SDGSpacing.kt
@@ -0,0 +1,30 @@
+package com.shopl.sdg_common.foundation.spacing
+
+import androidx.compose.ui.unit.dp
+
+/**
+ * SDG - Foundation - Spacing
+ *
+ * 컴포넌트 여백과 간격 값을 정의
+ *
+ * @see <a href="https://www.figma.com/design/qWVshatQ9eqoIn4fdEZqWy/SDG?node-id=6764-15774&m=dev">Figma</a>
+ */
+object SDGSpacing {
+    /** Common **/
+    val Spacing2 = 2.dp
+    val Spacing4 = 4.dp
+    val Spacing8 = 8.dp
+    val Spacing10 = 10.dp
+    val Spacing12 = 12.dp
+    val Spacing16 = 16.dp
+    val Spacing20 = 20.dp
+    val Spacing24 = 24.dp
+    val Spacing40 = 40.dp
+
+    /** Special **/
+    val Spacing6 = 6.dp
+    val Spacing28 = 28.dp
+    val Spacing32 = 32.dp
+    val Spacing100 = 100.dp
+    val Spacing104 = 104.dp
+}
