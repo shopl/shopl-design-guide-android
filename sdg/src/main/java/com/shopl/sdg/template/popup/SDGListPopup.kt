f082545b0e5eb6c6940d82245c4676e2bc757448
liz <liz.jo@shoplworks.com>
2025-07-18 15:46:19 +0900

    [SH-12068] List Popup > List Bottom Sheet 추가

--- sdg/src/main/java/com/shopl/sdg/template/popup/SDGListPopup.kt
+++ sdg/src/main/java/com/shopl/sdg/template/popup/SDGListPopup.kt
@@ -2,15 +2,25 @@ package com.shopl.sdg.template.popup
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.widthIn
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.ModalBottomSheet
+import androidx.compose.material3.SheetState
+import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.Stable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -18,14 +28,16 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalView
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Dialog
 import androidx.compose.ui.window.DialogProperties
 import androidx.compose.ui.window.DialogWindowProvider
 import com.shopl.sdg_common.ext.clickable
 import com.shopl.sdg_common.foundation.SDGColor
-import com.shopl.sdg_common.ui.components.IOText
+import com.shopl.sdg_common.foundation.typography.SDGTypography
+import com.shopl.sdg_common.ui.components.SDGText
+import kotlinx.collections.immutable.persistentListOf
 
 @Stable
 sealed class SDGListPopupItemUiState(
@@ -84,19 +96,126 @@ fun SDGListPopup(
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun SDGListBottomSheet(
+    modifier: Modifier = Modifier,
+    sheetState: SheetState = rememberModalBottomSheetState(
+        skipPartiallyExpanded = true
+    ),
+    onDismissRequest: () -> Unit,
+    body: @Composable () -> Unit,
+) {
+    ModalBottomSheet(
+        onDismissRequest = onDismissRequest,
+        sheetState = sheetState,
+        shape = RoundedCornerShape(
+            topStart = 20.dp,
+            topEnd = 20.dp
+        ),
+        containerColor = SDGColor.Neutral0,
+        dragHandle = null,
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(top = 60.dp)
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .navigationBarsPadding()
+        ) {
+            body()
+        }
+    }
+}
+
+
+@Composable
+fun SDGSelectableTextList(
+    modifier: Modifier = Modifier,
+    listContentPadding: PaddingValues = PaddingValues(bottom = 10.dp),
+    items: List<SDGListPopupItemUiState>,
+    onSelected: (SDGListPopupItemUiState) -> Unit,
+    onDismissRequest: () -> Unit,
+) {
+    val listState = rememberLazyListState()
+    LazyColumn(
+        state = listState,
+        modifier = modifier.fillMaxWidth(),
+        contentPadding = listContentPadding
+    ) {
+        itemsIndexed(items) { index, item ->
+            SDGListPopupItem(uiState = item) {
+                onSelected(item)
+                onDismissRequest()
+            }
+
+            if (index != items.lastIndex) {
+                HorizontalDivider(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 12.dp),
+                    color = SDGColor.Neutral200
+                )
+            }
+        }
+    }
+}
+
+
 @Composable
 fun SDGListPopupItem(
     uiState: SDGListPopupItemUiState,
     onClick: (SDGListPopupItemUiState) -> Unit,
 ) {
-    IOText(
+    SDGText(
         modifier = Modifier
             .fillMaxWidth()
             .clickable(true, rippleColor = SDGColor.Neutral350) { onClick(uiState) }
             .padding(vertical = 14.dp, horizontal = 20.dp),
         text = uiState.title,
         textColor = uiState.textColor,
-        fontSize = 16.sp,
+        typography = SDGTypography.Body1R,
         textAlign = TextAlign.Center,
     )
+}
+
+@Preview
+@Composable
+fun PreviewSDGListPopup() {
+    val popupItems = persistentListOf(
+        SDGListPopupItemUiState.Default("Default"),
+        SDGListPopupItemUiState.Selected("Selected"),
+        SDGListPopupItemUiState.Delete("Delete")
+    )
+    SDGListPopup(
+        items = popupItems,
+        onSelected = {},
+        onDismissRequest = {}
+    )
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Preview
+@Composable
+private fun PreviewSDGListBottomSheet() {
+    val sheetState = rememberModalBottomSheetState(
+        skipPartiallyExpanded = true
+    )
+    val popupItems = persistentListOf(
+        SDGListPopupItemUiState.Default("Default Text가 길어지면 상하 padding 유지한 상태로 줄바꿈하여 전체 노출 "),
+        SDGListPopupItemUiState.Selected("Selected"),
+        SDGListPopupItemUiState.Delete("Delete")
+    )
+    LaunchedEffect(Unit) { sheetState.expand() }
+    SDGListBottomSheet(
+        sheetState = sheetState,
+        onDismissRequest = {},
+    ) {
+        SDGSelectableTextList(
+            items = popupItems,
+            onSelected = {},
+            onDismissRequest = {}
+        )
+    }
 }
