206de1d817f19ec3772bf694173d327b6272ac55
Scott <thd0427__@naver.com>
2025-07-21 14:30:17 +0900

    SDG Spacing 적용

--- sdg/src/main/java/com/shopl/sdg/component/button/ghost/SDGGhostButtonSize.kt
+++ sdg/src/main/java/com/shopl/sdg/component/button/ghost/SDGGhostButtonSize.kt
@@ -2,6 +2,12 @@ package com.shopl.sdg.component.button.ghost
 
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import com.shopl.sdg_common.foundation.spacing.SDGSpacing.Spacing10
+import com.shopl.sdg_common.foundation.spacing.SDGSpacing.Spacing16
+import com.shopl.sdg_common.foundation.spacing.SDGSpacing.Spacing2
+import com.shopl.sdg_common.foundation.spacing.SDGSpacing.Spacing4
+import com.shopl.sdg_common.foundation.spacing.SDGSpacing.Spacing6
+import com.shopl.sdg_common.foundation.spacing.SDGSpacing.Spacing8
 import com.shopl.sdg_common.foundation.typography.SDGTypography
 
 sealed class SDGGhostButtonSize(
@@ -13,24 +19,24 @@ sealed class SDGGhostButtonSize(
 ) {
     data object Large : SDGGhostButtonSize(
         height = 42.dp,
-        horizontalPadding = 16.dp,
-        gap = 6.dp,
+        horizontalPadding = Spacing16,
+        gap = Spacing6,
         typography = SDGTypography.Body1R,
         rippleRadius = 14.dp,
     )
 
     data object Medium : SDGGhostButtonSize(
         height = 36.dp,
-        horizontalPadding = 10.dp,
-        gap = 4.dp,
+        horizontalPadding = Spacing10,
+        gap = Spacing4,
         typography = SDGTypography.Body2R,
         rippleRadius = 12.dp,
     )
 
     data object Small : SDGGhostButtonSize(
         height = 28.dp,
-        horizontalPadding = 8.dp,
-        gap = 2.dp,
+        horizontalPadding = Spacing8,
+        gap = Spacing2,
         typography = SDGTypography.Body3R,
         rippleRadius = 9.dp,
     )
