70d028e5d5b9d83c8f0a8e77be353b19af74c7ba
Scott <thd0427__@naver.com>
2025-07-29 12:58:23 +0900

    불필요 import 제거

--- sdg/src/main/java/com/shopl/sdg/component/button/ghost/SDGGhostButtonIconSize.kt
+++ sdg/src/main/java/com/shopl/sdg/component/button/ghost/SDGGhostButtonIconSize.kt
@@ -3,9 +3,6 @@ package com.shopl.sdg.component.button.ghost
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 
-/**
- * Ghost Button Icon Size
- */
 enum class SDGGhostButtonIconSize(val size: Dp) {
     Icon16(size = 16.dp),
     Icon14(size = 14.dp),
--- sdg/src/main/java/com/shopl/sdg/template/multi_calendar/SDGMultiCalendar.kt
+++ sdg/src/main/java/com/shopl/sdg/template/multi_calendar/SDGMultiCalendar.kt
@@ -33,7 +33,6 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.shopl.sdg.component.button.SDGButtonFontWeight
 import com.shopl.sdg.component.button.ghost.SDGGhostButtonSize
 import com.shopl.sdg.component.calendar.SDGCalendarDay
 import com.shopl.sdg.component.calendar.SDGCalendarDayMode
