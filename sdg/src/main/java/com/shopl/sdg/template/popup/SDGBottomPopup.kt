97f787687a3fcbe6163c1eb7f0dd3230fa785add
willy <willy.jung@shoplworks.com>
2025-08-06 18:42:42 +0900

    바텀 팝업 UI 수정

--- sdg/src/main/java/com/shopl/sdg/template/popup/SDGBottomPopup.kt
+++ sdg/src/main/java/com/shopl/sdg/template/popup/SDGBottomPopup.kt
@@ -14,10 +14,20 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.unit.dp
 import com.shopl.sdg_common.foundation.SDGColor
+import com.shopl.sdg_common.foundation.SDGCornerRadius
+import com.shopl.sdg_common.foundation.spacing.SDGSpacing
 import com.shopl.sdg_resource.R
 
+/**
+ * SDG - Template - Popup - Bottom Popup
+ *
+ * 태스크 수행을 위한 콘텐츠를 포함하는 화면 하단에 노출되는 팝업
+ *
+ * @param inputState SimpeInput 에서 사용되는 state, Error인 경우 현재 message 별도 출력하지 않음
+ *
+ * @see <a href="https://www.figma.com/design/qWVshatQ9eqoIn4fdEZqWy/SDG?node-id=19210-2895&m=dev">Figma</a>
+ */
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SDGBottomPopup(
@@ -38,7 +48,10 @@ fun SDGBottomPopup(
         onDismissRequest = onClickCancel ?: onClickConfirm,
         modifier = modifier,
         sheetState = sheetState,
-        shape = RoundedCornerShape(topStart = 20.dp, topEnd = 20.dp),
+        shape = RoundedCornerShape(
+            topStart = SDGCornerRadius.Radius20,
+            topEnd = SDGCornerRadius.Radius20
+        ),
         containerColor = containerColor,
         contentColor = contentColor,
         scrimColor = SDGColor.Neutral900_a40,
@@ -53,8 +66,10 @@ fun SDGBottomPopup(
                 modifier = Modifier
                     .fillMaxWidth()
                     .padding(
-                        vertical = 32.dp,
-                        horizontal = 28.dp,
+                        start = SDGSpacing.Spacing24,
+                        top = SDGSpacing.Spacing24,
+                        end = SDGSpacing.Spacing24,
+                        bottom = SDGSpacing.Spacing28
                     ),
             ) {
                 content()
