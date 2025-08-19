323edfd144ae4fe441c855a2d9e9be0f34ac0b24
willy <willy.jung@shoplworks.com>
2025-08-20 08:42:12 +0900

    [Android] SDG Template > Simple Text Form - 숫자만 사용하는 경우 키보드 타입 수정

--- sdg/src/main/java/com/shopl/sdg/template/util/simple_input_form/SDGSimpleInputForm.kt
+++ sdg/src/main/java/com/shopl/sdg/template/util/simple_input_form/SDGSimpleInputForm.kt
@@ -12,6 +12,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -22,6 +23,8 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -136,7 +139,11 @@ fun SDGSimpleInputForm(
             onInputChange = onValueChange,
             decimalFormat = decimalFormat,
             minValue = minValue,
-            maxValue = maxValue
+            maxValue = maxValue,
+            keyboardOptions = KeyboardOptions.Default.copy(
+                imeAction = ImeAction.Done,
+                keyboardType = KeyboardType.Number
+            )
         )
     }
 }
