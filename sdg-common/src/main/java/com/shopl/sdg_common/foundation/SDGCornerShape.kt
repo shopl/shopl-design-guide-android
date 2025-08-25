1f3f128de5dca57ba847e13681ac60d96a8c98eb
willy <willy.jung@shoplworks.com>
2025-08-25 09:42:53 +0900

    SDGCornerRadius Deprecated

--- sdg-common/src/main/java/com/shopl/sdg_common/foundation/SDGCornerShape.kt
+++ sdg-common/src/main/java/com/shopl/sdg_common/foundation/SDGCornerShape.kt
@@ -1,15 +1,16 @@
 package com.shopl.sdg_common.foundation
 
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.ui.unit.dp
 
 /**
  * SDG - Foundation - Corner Radius(RoundedCornerShape)
  * 화면 및 카드의 Radius가 반영된 RoundedCornerShape
  */
 object SDGCornerShape {
-    val Radius4 = RoundedCornerShape(SDGCornerRadius.Radius4)
-    val Radius8 = RoundedCornerShape(SDGCornerRadius.Radius8)
-    val Radius12 = RoundedCornerShape(SDGCornerRadius.Radius12)
-    val Radius16 = RoundedCornerShape(SDGCornerRadius.Radius16)
-    val Radius20 = RoundedCornerShape(SDGCornerRadius.Radius20)
+    val Radius4 = RoundedCornerShape(4.dp)
+    val Radius8 = RoundedCornerShape(8.dp)
+    val Radius12 = RoundedCornerShape(12.dp)
+    val Radius16 = RoundedCornerShape(16.dp)
+    val Radius20 = RoundedCornerShape(20.dp)
 }
