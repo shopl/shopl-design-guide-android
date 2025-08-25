b4ad29b3e13f271eff9c658622957662a2f2b9e9
willy <willy.jung@shoplworks.com>
2025-08-25 09:37:03 +0900

    SDGCornerShape 정의 추가

---
+++ sdg-common/src/main/java/com/shopl/sdg_common/foundation/SDGCornerShape.kt
@@ -0,0 +1,15 @@
+package com.shopl.sdg_common.foundation
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+
+/**
+ * SDG - Foundation - Corner Radius(RoundedCornerShape)
+ * 화면 및 카드의 Radius가 반영된 RoundedCornerShape
+ */
+object SDGCornerShape {
+    val Radius4 = RoundedCornerShape(SDGCornerRadius.Radius4)
+    val Radius8 = RoundedCornerShape(SDGCornerRadius.Radius8)
+    val Radius12 = RoundedCornerShape(SDGCornerRadius.Radius12)
+    val Radius16 = RoundedCornerShape(SDGCornerRadius.Radius16)
+    val Radius20 = RoundedCornerShape(SDGCornerRadius.Radius20)
+}
