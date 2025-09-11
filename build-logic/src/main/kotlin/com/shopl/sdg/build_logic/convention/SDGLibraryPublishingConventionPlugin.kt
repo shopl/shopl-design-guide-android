88da717606ffd1c75a9980dce7f30a36875ac0a0
Scott <thd0427__@naver.com>
2025-09-11 13:19:54 +0900

    서명 로직 추가

--- build-logic/src/main/kotlin/com/shopl/sdg/build_logic/convention/SDGLibraryPublishingConventionPlugin.kt
+++ build-logic/src/main/kotlin/com/shopl/sdg/build_logic/convention/SDGLibraryPublishingConventionPlugin.kt
@@ -3,8 +3,8 @@ package com.shopl.sdg.build_logic.convention
 import com.shopl.sdg.build_logic.PublishingConfig.GROUP
 import com.shopl.sdg.build_logic.PublishingConfig.VERSION
 import com.shopl.sdg.build_logic.libs
+import com.vanniktech.maven.publish.AndroidSingleVariantLibrary
 import com.vanniktech.maven.publish.MavenPublishBaseExtension
-import com.vanniktech.maven.publish.SonatypeHost
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.kotlin.dsl.configure
@@ -18,45 +18,55 @@ class SDGLibraryPublishingConventionPlugin : Plugin<Project> {
             apply(libs.findPlugin("vanniktech.maven").get().get().pluginId)
         }
 
-        afterEvaluate {
-            extensions.configure<MavenPublishBaseExtension> {
-                val publishArtifact = (target.findProperty("artifactId") as? String)
+//        afterEvaluate {
+        extensions.configure<MavenPublishBaseExtension> {
+            //                val publishArtifact = (target.findProperty("artifactId") as? String)
+            val publishArtifact = providers.gradleProperty("artifactId")
+                .orElse(name)
+                .get()
+//                publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
+            publishToMavenCentral(automaticRelease = true)
 
-                publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
-                signAllPublications()
-                coordinates(
-                    GROUP,
-                    publishArtifact,
-                    VERSION
+            coordinates(
+                GROUP,
+                publishArtifact,
+                VERSION
+            )
+            configure(
+                AndroidSingleVariantLibrary(
+                    variant = "release",
+                    sourcesJar = true,
+                    publishJavadocJar = true
                 )
+            )
 
-                pom {
-                    name.set("Shopl-Design-Guide")
-                    description.set("SDG is a collection of design resources, reusable components and guidelines for creating Shopl's products.")
+            pom {
+                name.set("Shopl-Design-Guide")
+                description.set("SDG is a collection of design resources, reusable components and guidelines for creating Shopl's products.")
+                url.set("https://github.com/shopl/shopl-design-guide-android")
+                inceptionYear.set("2025")
+
+                licenses {
+                    license {
+                        name.set("The Apache License, Version 2.0")
+                        url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("shopl-dev")
+                        name.set("shopl-dev")
+                        url.set("https://github.com/shopl-dev")
+                    }
+                }
+
+                scm {
                     url.set("https://github.com/shopl/shopl-design-guide-android")
-                    inceptionYear.set("2025")
-
-                    licenses {
-                        license {
-                            name.set("The Apache License, Version 2.0")
-                            url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
-                        }
-                    }
-                    developers {
-                        developer {
-                            id.set("shopl-dev")
-                            name.set("shopl-dev")
-                            url.set("https://github.com/shopl-dev")
-                        }
-                    }
-
-                    scm {
-                        url.set("https://github.com/shopl/shopl-design-guide-android")
-                        connection.set("scm:git:git://github.com/shopl/shopl-design-guide-android.git")
-                        developerConnection.set("scm:git:ssh://git@github.com:shopl/shopl-design-guide-android.git")
-                    }
+                    connection.set("scm:git:git://github.com/shopl/shopl-design-guide-android.git")
+                    developerConnection.set("scm:git:ssh://git@github.com:shopl/shopl-design-guide-android.git")
                 }
             }
         }
+//        }
     }
 }
