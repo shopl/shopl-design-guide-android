e480eab947926c5b4b58f59cce56889331133600
Scott <thd0427__@naver.com>
2025-09-23 13:11:20 +0900

    type property 수정

--- sdg/src/main/java/com/shopl/sdg/component/time_picker/SDGTimePickerType.kt
+++ sdg/src/main/java/com/shopl/sdg/component/time_picker/SDGTimePickerType.kt
@@ -3,9 +3,6 @@ package com.shopl.sdg.component.time_picker
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 
-internal val DEFAULT_TIME_PICKER_TWO_OPTION_SPACING = 4.dp
-internal val DEFAULT_TIME_PICKER_DIVIDER_HORIZONTAL_PADDING = 8.dp
-
 sealed interface SDGTimePickerOption {
     data class OneOption(
         val value: Int,
@@ -15,11 +12,8 @@ sealed interface SDGTimePickerOption {
     ) : SDGTimePickerOption
 
     data class TwoOption(
-        val first: SDGTimePickerColumnOption,
-        val second: SDGTimePickerColumnOption,
-        val dividerText: String = ":",
-        val spacing: Dp = DEFAULT_TIME_PICKER_TWO_OPTION_SPACING,
-        val dividerHorizontalPadding: Dp = DEFAULT_TIME_PICKER_DIVIDER_HORIZONTAL_PADDING,
+        val left: SDGTimePickerColumnOption,
+        val right: SDGTimePickerColumnOption,
     ) : SDGTimePickerOption
 }
 
@@ -28,5 +22,4 @@ data class SDGTimePickerColumnOption(
     val range: IntRange,
     val onValueChange: (Int) -> Unit,
     val width: Dp = 0.dp,
-    val isEditMode: Boolean = false,
 )
