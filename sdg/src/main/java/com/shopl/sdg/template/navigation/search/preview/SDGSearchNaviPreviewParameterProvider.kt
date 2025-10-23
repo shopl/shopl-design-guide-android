34df3cd357dd2c79183833af2f9e66b2ebfe78c1
Scott <thd0427__@naver.com>
2025-10-23 09:48:50 +0900

    preview 추가

---
+++ sdg/src/main/java/com/shopl/sdg/template/navigation/search/preview/SDGSearchNaviPreviewParameterProvider.kt
@@ -0,0 +1,38 @@
+package com.shopl.sdg.template.navigation.search.preview
+
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.PreviewParameterProvider
+import com.shopl.sdg.template.navigation.search.SDGSearchNaviType
+import com.shopl.sdg_common.foundation.SDGColor
+
+internal class SDGSearchNaviPreviewParameterProvider :
+    PreviewParameterProvider<SDGSearchNaviPreviewData> {
+    override val values: Sequence<SDGSearchNaviPreviewData> = sequenceOf(
+        SDGSearchNaviPreviewData(
+            type = SDGSearchNaviType.Full(
+                onClickClose = {},
+                iconColor = SDGColor.Neutral0
+            ),
+            input = "베스트 상품",
+            hint = "검색어를 입력하세요",
+            backgroundColor = SDGColor.Secondary300
+        ),
+        SDGSearchNaviPreviewData(
+            type = SDGSearchNaviType.Back(
+                onClickBack = {},
+                iconColor = SDGColor.Neutral0
+            ),
+            input = "",
+            hint = "브랜드를 검색해보세요",
+            backgroundColor = SDGColor.Primary300
+        )
+    )
+}
+
+internal data class SDGSearchNaviPreviewData(
+    val type: SDGSearchNaviType,
+    val input: String,
+    val hint: String,
+    val backgroundColor: Color
+)
+
