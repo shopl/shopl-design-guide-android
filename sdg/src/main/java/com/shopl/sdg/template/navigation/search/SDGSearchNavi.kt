35e276059bcf7ac567d2bbe1f6967875de33f531
Scott <thd0427__@naver.com>
2025-10-23 09:07:27 +0900

    SDGSearchNavi 추가

---
+++ sdg/src/main/java/com/shopl/sdg/template/navigation/search/SDGSearchNavi.kt
@@ -0,0 +1,97 @@
+package com.shopl.sdg.template.navigation.search
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import com.shopl.sdg.component.search_bar.capsule.SDGCapsuleSearch
+import com.shopl.sdg.component.search_bar.capsule.SDGCapsuleSearchType
+import com.shopl.sdg_common.ext.clickable
+import com.shopl.sdg_common.ui.components.SDGImage
+import com.shopl.sdg_resource.R
+
+/**
+ * SDG - Navigation - Search Navi
+ *
+ * 화면의 상단에 위치하며, 검색바와 조합된 템플릿
+ *
+ * @param
+ *
+ * @see <a href="https://www.figma.com/design/qWVshatQ9eqoIn4fdEZqWy/SDG?node-id=19821-30930&t=6UHqf2CQvDUAo0we-4">Figma</a>
+ */
+@Composable
+fun SDGSearchNavi(
+    type: SDGSearchNaviType,
+    input: String,
+    hint: String,
+    backgroundColor: Color,
+    onInputChange: (String) -> Unit,
+    onDeleteClick: () -> Unit,
+    useStartRequester: Boolean = true,
+    keyboardOptions: KeyboardOptions = KeyboardOptions.Default,
+    keyboardActions: KeyboardActions = KeyboardActions.Default,
+) {
+    Row(
+        modifier = Modifier
+            .background(backgroundColor)
+            .height(48.dp)
+            .fillMaxWidth()
+            .padding(horizontal = 10.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        when (type) {
+            is SDGSearchNaviType.Back -> {
+                SDGImage(
+                    modifier = Modifier
+                        .size(40.dp)
+                        .clickable(false) { type.onClickBack() },
+                    resId = R.drawable.ic_navi_back_android,
+                    color = type.iconColor,
+                )
+                SDGCapsuleSearch(
+                    type = SDGCapsuleSearchType.Solid,
+                    input = input,
+                    hint = hint,
+                    enabled = true,
+                    onInputChange = onInputChange,
+                    onDeleteClick = onDeleteClick,
+                    useStartRequester = useStartRequester,
+                    keyboardOptions = keyboardOptions,
+                    keyboardActions = keyboardActions,
+                )
+            }
+
+            is SDGSearchNaviType.Full -> {
+                SDGCapsuleSearch(
+                    type = SDGCapsuleSearchType.Solid,
+                    input = input,
+                    hint = hint,
+                    enabled = true,
+                    onInputChange = onInputChange,
+                    onDeleteClick = onDeleteClick,
+                    useStartRequester = useStartRequester,
+                    keyboardOptions = keyboardOptions,
+                    keyboardActions = keyboardActions,
+                )
+
+                SDGImage(
+                    modifier = Modifier
+                        .size(40.dp)
+                        .clickable(false) { type.onClickClose() },
+                    resId = R.drawable.ic_navi_close,
+                    color = type.iconColor,
+                )
+            }
+        }
+    }
+}
+
