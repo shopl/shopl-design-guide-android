225ddc79660d09865116ddd70ac7ed5f053cc3bd
Scott <thd0427__@naver.com>
2025-10-23 09:48:42 +0900

    SDGCapsuleSearch weight 적용

--- sdg/src/main/java/com/shopl/sdg/template/navigation/search/SDGSearchNavi.kt
+++ sdg/src/main/java/com/shopl/sdg/template/navigation/search/SDGSearchNavi.kt
@@ -12,9 +12,13 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.unit.dp
-import com.shopl.sdg.component.search_bar.capsule.SDGCapsuleSearch
 import com.shopl.sdg.component.search_bar.capsule.SDGCapsuleSearchType
+import com.shopl.sdg.component.util.search_bar.capsule.SDGCapsuleSearch
+import com.shopl.sdg.template.navigation.search.preview.SDGSearchNaviPreviewData
+import com.shopl.sdg.template.navigation.search.preview.SDGSearchNaviPreviewParameterProvider
 import com.shopl.sdg_common.ext.clickable
 import com.shopl.sdg_common.ui.components.SDGImage
 
@@ -22,8 +26,6 @@ import com.shopl.sdg_common.ui.components.SDGImage
  * SDG - Navigation - Search Navi
  *
  * 화면의 상단에 위치하며, 검색바와 조합된 템플릿
- *
- * @param
  *
  * @see <a href="https://www.figma.com/design/qWVshatQ9eqoIn4fdEZqWy/SDG?node-id=19821-30930&t=6UHqf2CQvDUAo0we-4">Figma</a>
  */
@@ -52,6 +54,7 @@ fun SDGSearchNavi(
         }
 
         SDGCapsuleSearch(
+            weight = 1f,
             type = SDGCapsuleSearchType.Solid,
             input = input,
             hint = hint,
@@ -77,5 +80,22 @@ private fun SearchNaviIconButton(icon: SDGSearchNaviIcon) {
             .clickable(false) { icon.onClick() },
         resId = icon.iconResId,
         color = icon.iconColor,
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun PreviewSDGSearchNavi(
+    @PreviewParameter(SDGSearchNaviPreviewParameterProvider::class)
+    previewData: SDGSearchNaviPreviewData
+) {
+    SDGSearchNavi(
+        type = previewData.type,
+        input = previewData.input,
+        hint = previewData.hint,
+        backgroundColor = previewData.backgroundColor,
+        onInputChange = {},
+        onDeleteClick = {},
+        useStartRequester = false
     )
 }
