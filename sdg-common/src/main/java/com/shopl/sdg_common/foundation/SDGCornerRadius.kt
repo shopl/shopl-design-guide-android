dd6eb3b488fa0b090d8a315641b7d3729716a901
willy <willy.jung@shoplworks.com>
2025-10-27 15:32:28 +0900

    Radius 추가

--- sdg-common/src/main/java/com/shopl/sdg_common/foundation/SDGCornerRadius.kt
+++ sdg-common/src/main/java/com/shopl/sdg_common/foundation/SDGCornerRadius.kt
@@ -25,11 +25,28 @@ object SDGCornerRadius {
      * 20이 기본이며, 콘텐츠의 양에 따라 16/12, 8/4 사용
      */
     object BoxRadius {
+        /**
+         * Common 기본 단위
+         */
         val Radius4 = RoundedCornerShape(4.dp)
         val Radius8 = RoundedCornerShape(8.dp)
         val Radius12 = RoundedCornerShape(12.dp)
         val Radius16 = RoundedCornerShape(16.dp)
         val Radius20 = RoundedCornerShape(20.dp)
+
+        /**
+         * Special 예외 단위
+         */
+        val Radius2 = RoundedCornerShape(2.dp)
+        val Radius6 = RoundedCornerShape(6.dp)
+        val Radius10 = RoundedCornerShape(10.dp)
+
+        /**
+         * Etc 일부 컴포넌트에 사용된 단위
+         */
+        internal val Radius14 = RoundedCornerShape(14.dp)
+        internal val Radius18 = RoundedCornerShape(18.dp)
+        internal val Radius25 = RoundedCornerShape(25.dp)
     }
 
 }
