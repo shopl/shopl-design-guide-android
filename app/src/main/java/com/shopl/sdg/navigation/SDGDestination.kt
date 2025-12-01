4ad251ee0c63703fbd70ac3cb5cb606adb30142f
Scott <thd0427__@naver.com>
2025-12-01 10:14:41 +0900

    [SH-15587] destination 구조 정의 (#202)

---
+++ app/src/main/java/com/shopl/sdg/navigation/SDGDestination.kt
@@ -0,0 +1,18 @@
+package com.shopl.sdg.navigation
+
+import kotlinx.serialization.Serializable
+
+/**
+ * 세 가지 주요 카테고리로 구성
+ * - Foundation: 디자인 시스템의 기본 요소 (Color, Typography 등)
+ * - Component: UI 컴포넌트들 (Button, Avatar 등)
+ * - Template: 컴포넌트들이 조합된 템플릿
+ */
+sealed interface SDGDestination {
+    
+    /**
+     * 홈 화면
+     */
+    @Serializable
+    data object Home : SDGDestination
+}
---
+++ app/src/main/java/com/shopl/sdg/navigation/TemplateDestination.kt
@@ -0,0 +1,132 @@
+package com.shopl.sdg.navigation
+
+import androidx.compose.runtime.Stable
+import kotlinx.serialization.Serializable
+
+/**
+ * SDG - Template
+ *
+ * @see <a href="https://www.figma.com/design/qWVshatQ9eqoIn4fdEZqWy/SDG?node-id=16930-2151&m=dev">Figma</a>
+ */
+@Stable
+sealed interface TemplateDestination {
+    @Serializable
+    data object CalendarAndTime : TemplateDestination
+
+    @Serializable
+    data object CheckboxLabel : TemplateDestination
+
+    @Serializable
+    data object CheckOptionLabel : TemplateDestination
+
+    @Serializable
+    data object EmptyImg : TemplateDestination
+
+    @Stable
+    sealed interface FilterChip : TemplateDestination {
+        @Serializable
+        data object NaviFilterChip : FilterChip
+
+        @Serializable
+        data object BodyFilterChip : FilterChip
+    }
+
+    @Stable
+    sealed interface Form : TemplateDestination {
+        @Serializable
+        data object DropdownForm : Form
+
+        @Serializable
+        data object SelectForm : Form
+
+        @Serializable
+        data object SimpleTextForm : Form
+
+        @Serializable
+        data object FixedTextForm : Form
+
+        @Serializable
+        data object TimeSelectForm : Form
+    }
+
+    @Stable
+    sealed interface FoundationList : TemplateDestination {
+        @Serializable
+        data object UserList : FoundationList
+
+        @Serializable
+        data object WorkplaceList : FoundationList
+
+        @Serializable
+        data object GroupAndPositionList : FoundationList
+    }
+
+    @Serializable
+    data object History : TemplateDestination
+
+    @Serializable
+    data object HistoryMini : TemplateDestination
+
+    @Serializable
+    data object ListHeader : TemplateDestination
+
+    @Serializable
+    data object MultiCalendar : TemplateDestination
+
+    @Serializable
+    data object MultiTimePicker : TemplateDestination
+
+    @Stable
+    sealed interface Navigation : TemplateDestination {
+        @Serializable
+        data object BasicNavi : Navigation
+
+        @Serializable
+        data object CategoryNavi : Navigation
+
+        @Serializable
+        data object TextNavi : Navigation
+
+        @Serializable
+        data object SearchNavi : Navigation
+    }
+
+    @Stable
+    sealed interface Popup : TemplateDestination {
+        @Serializable
+        data object CenterPopup : Popup
+
+        @Serializable
+        data object BottomPopup : Popup
+
+        @Serializable
+        data object ListPopup : Popup
+
+        @Serializable
+        data object MiniListPopup : Popup
+
+        @Serializable
+        data object IconPopup : Popup
+
+        @Serializable
+        data object ToastPopup : Popup
+    }
+
+    @Stable
+    sealed interface Profile : TemplateDestination {
+        @Serializable
+        data object BasicProfile : Profile
+
+        @Serializable
+        data object SecondProfile : Profile
+
+        @Serializable
+        data object SimpleProfile : Profile
+
+        @Serializable
+        data object MiniProfile : Profile
+    }
+
+    @Serializable
+    data object RadioLabel : TemplateDestination
+}
