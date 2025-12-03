d168ec9b9994e6820d447ca602757103c9f60301
Scott <thd0427__@naver.com>
2025-12-03 17:50:35 +0900

    좌측 아이콘 및 텍스트 영역 구현

--- sdg/src/main/java/com/shopl/sdg/template/list_header/icon/SDGIconHeader.kt
+++ sdg/src/main/java/com/shopl/sdg/template/list_header/icon/SDGIconHeader.kt
@@ -1,11 +1,13 @@
 package com.shopl.sdg.component.header.icon
 
+import androidx.annotation.DrawableRes
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -13,7 +15,11 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import com.shopl.sdg_common.foundation.SDGColor
 import com.shopl.sdg_common.foundation.spacing.SDGSpacing.Spacing12
+import com.shopl.sdg_common.foundation.spacing.SDGSpacing.Spacing3
 import com.shopl.sdg_common.foundation.spacing.SDGSpacing.Spacing4
+import com.shopl.sdg_common.foundation.typography.SDGTypography
+import com.shopl.sdg_common.ui.components.SDGImage
+import com.shopl.sdg_common.ui.components.SDGText
 
 /**
  * SDG - List Header - Icon Header
@@ -23,8 +29,11 @@ import com.shopl.sdg_common.foundation.spacing.SDGSpacing.Spacing4
 @Composable
 fun SDGIconHeader(
     label: String,
+    count: String? = null,
     backgroundColor: Color = SDGColor.Transparent,
     marginValues: PaddingValues = PaddingValues(),
+    @DrawableRes iconResId: Int? = null,
+    iconColor: Color? = null,
 ) {
     Row(
         modifier = Modifier
@@ -34,6 +43,32 @@ fun SDGIconHeader(
         verticalAlignment = Alignment.CenterVertically,
         horizontalArrangement = Arrangement.spacedBy(Spacing12)
     ) {
-        // TODO: Implement Content
+        Row(
+            modifier = Modifier.weight(1f, false),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.spacedBy(Spacing3)
+        ) {
+            if (iconResId != null && iconColor != null) {
+                SDGImage(
+                    modifier = Modifier.size(24.dp),
+                    resId = iconResId,
+                    color = iconColor
+                )
+            }
+
+            SDGText(
+                text = label,
+                textColor = SDGColor.Neutral700,
+                typography = SDGTypography.Body1SB
+            )
+
+            if (count != null) {
+                SDGText(
+                    text = count,
+                    textColor = SDGColor.Neutral700,
+                    typography = SDGTypography.Body1SB
+                )
+            }
+        }
     }
 }
