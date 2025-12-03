2ac5e56fb2f53dc8148642a77e8ffab06fee24ae
Scott <thd0427__@naver.com>
2025-12-03 17:50:54 +0900

    우측 아이콘 영역 및 스타일링 적용

--- sdg/src/main/java/com/shopl/sdg/template/list_header/icon/SDGIconHeader.kt
+++ sdg/src/main/java/com/shopl/sdg/template/list_header/icon/SDGIconHeader.kt
@@ -2,21 +2,27 @@ package com.shopl.sdg.component.header.icon
 
 import androidx.annotation.DrawableRes
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.VerticalDivider
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import com.shopl.sdg_common.foundation.SDGColor
+import com.shopl.sdg_common.foundation.SDGCornerRadius
 import com.shopl.sdg_common.foundation.spacing.SDGSpacing.Spacing12
 import com.shopl.sdg_common.foundation.spacing.SDGSpacing.Spacing3
 import com.shopl.sdg_common.foundation.spacing.SDGSpacing.Spacing4
+import com.shopl.sdg_common.foundation.spacing.SDGSpacing.Spacing8
 import com.shopl.sdg_common.foundation.typography.SDGTypography
 import com.shopl.sdg_common.ui.components.SDGImage
 import com.shopl.sdg_common.ui.components.SDGText
@@ -25,6 +31,8 @@ import com.shopl.sdg_common.ui.components.SDGText
  * SDG - List Header - Icon Header
  *
  * 리스트 최상단에 표시되는 정보와 아이콘이 조합된 템플릿
+ *
+ * @see <a href="https://www.figma.com/design/qWVshatQ9eqoIn4fdEZqWy/SDG?node-id=21463-14546&m=dev">Figma</a>
  */
 @Composable
 fun SDGIconHeader(
@@ -34,6 +42,9 @@ fun SDGIconHeader(
     marginValues: PaddingValues = PaddingValues(),
     @DrawableRes iconResId: Int? = null,
     iconColor: Color? = null,
+    iconHeaderRightItem: SDGIconHeaderRightItem? = null,
+    onLeftIconClick: (() -> Unit)? = null,
+    onRightIconClick: ((Int) -> Unit)? = null,
 ) {
     Row(
         modifier = Modifier
@@ -68,6 +79,45 @@ fun SDGIconHeader(
                     textColor = SDGColor.Neutral700,
                     typography = SDGTypography.Body1SB
                 )
+            }
+        }
+
+        if (iconHeaderRightItem != null) {
+            Row(
+                modifier = Modifier
+                    .then(
+                        if (iconHeaderRightItem.isBox) {
+                            Modifier
+                                .border(
+                                    width = 1.dp,
+                                    color = SDGColor.Neutral200,
+                                    shape = SDGCornerRadius.BoxRadius.Radius6
+                                )
+                                .padding(horizontal = 8.dp, vertical = 4.dp)
+                        } else {
+                            Modifier
+                                .padding(horizontal = 8.dp)
+                        }
+                    ),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.spacedBy(Spacing8)
+            ) {
+                iconHeaderRightItem.icons.forEachIndexed { index, (resId, color) ->
+                    SDGImage(
+                        modifier = Modifier.size(20.dp),
+                        resId = resId,
+                        color = color
+                    )
+
+                    if (iconHeaderRightItem.icons.size > 1 && index != iconHeaderRightItem.icons.lastIndex) {
+                        VerticalDivider(
+                            modifier = Modifier
+                                .width(1.dp)
+                                .height(14.dp),
+                            color = SDGColor.Neutral200
+                        )
+                    }
+                }
             }
         }
     }
