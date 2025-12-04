c499fbb0dc8c2d1c0a0c93013fc66beb75c74ecc
Scott <thd0427__@naver.com>
2025-12-04 13:33:15 +0900

    SDGScrollTabPreviewParameterProvider 추가

---
+++ sdg/src/main/java/com/shopl/sdg/component/tab/scroll/SDGScrollTabPreviewParameterProvider.kt
@@ -0,0 +1,29 @@
+package com.shopl.sdg.component.tab.scroll
+
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.ui.tooling.preview.PreviewParameterProvider
+import androidx.compose.ui.unit.dp
+import kotlinx.collections.immutable.PersistentList
+import kotlinx.collections.immutable.persistentListOf
+
+internal class SDGScrollTabPreviewParameterProvider :
+    PreviewParameterProvider<SDGScrollTabPreviewParameter> {
+
+    override val values: Sequence<SDGScrollTabPreviewParameter> = sequenceOf(
+        SDGScrollTabPreviewParameter(
+            titles = persistentListOf("Label", "Label", "Label", "Label", "Label"),
+            selectedTabIndex = 0,
+        ),
+        SDGScrollTabPreviewParameter(
+            titles = persistentListOf("전체", "진행중", "완료", "보류", "취소"),
+            selectedTabIndex = 2,
+            marginValues = PaddingValues(horizontal = 16.dp)
+        ),
+    )
+}
+
+internal data class SDGScrollTabPreviewParameter(
+    val titles: PersistentList<String>,
+    val selectedTabIndex: Int,
+    val marginValues: PaddingValues = PaddingValues(0.dp),
+)
