425d854fbb6d40bca3ac69bde6a6fdc83eab9c08
Scott <thd0427__@naver.com>
2025-12-05 15:08:30 +0900

    테스트 데이터

--- app/src/main/java/com/shopl/sdg/MainActivity.kt
+++ app/src/main/java/com/shopl/sdg/MainActivity.kt
@@ -10,17 +10,15 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateListOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.navigation3.runtime.NavEntry
+import androidx.navigation3.runtime.entryProvider
+import androidx.navigation3.runtime.rememberNavBackStack
 import androidx.navigation3.ui.NavDisplay
 import com.shopl.sdg.navigation.ComponentDestination
 import com.shopl.sdg.navigation.SDGDestination
 import com.shopl.sdg.tmp.AvatarScreen
 import com.shopl.sdg.tmp.BoxBadgeScreen
 import com.shopl.sdg.tmp.CapsuleBadgeScreen
-import com.shopl.sdg.tmp.EmptyScreen
 import com.shopl.sdg.tmp.HomeScreen
 import com.shopl.sdg.ui.theme.ShoplDesignGuideTheme
 import com.shopl.sdg_common.foundation.SDGColor
@@ -41,45 +39,71 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 private fun NavigationBasic(innerPadding: PaddingValues) {
-    val backStack = remember { mutableStateListOf<SDGDestination>(SDGDestination.Home) }
+    val backStack = rememberNavBackStack(SDGDestination.Home)
+
     NavDisplay(
         modifier = Modifier
             .padding(innerPadding)
             .background(SDGColor.GreenG),
         backStack = backStack,
         onBack = { backStack.removeLastOrNull() },
-        entryProvider = { key ->
-            when (key) {
-                SDGDestination.Home -> NavEntry(key) {
-                    HomeScreen(
-                        onClick = { backStack.add(ComponentDestination.Avatar as ComponentDestination as SDGDestination) }
-                    )
-                }
+        entryProvider = entryProvider {
+            entry<SDGDestination.Home> {
+                HomeScreen(
+                    onClick = { backStack.add(ComponentDestination.Avatar as ComponentDestination as SDGDestination) }
+                )
+            }
 
-                ComponentDestination.Avatar -> NavEntry(key) {
-                    AvatarScreen(
-                        onClick = { backStack.add(ComponentDestination.Badge.CapsuleBadge as ComponentDestination.Badge as SDGDestination) }
-                    )
-                }
+            entry<ComponentDestination.Avatar> {
+                AvatarScreen(
+                    onClick = { backStack.add(ComponentDestination.Badge.CapsuleBadge as ComponentDestination.Badge as SDGDestination) }
+                )
+            }
 
-                ComponentDestination.Badge.CapsuleBadge -> NavEntry(key) {
-                    CapsuleBadgeScreen(
-                        onClick = { backStack.add(ComponentDestination.Badge.BoxBadge as ComponentDestination.Badge as SDGDestination) }
-                    )
-                }
+            entry<ComponentDestination.Badge.CapsuleBadge> {
+                CapsuleBadgeScreen(
+                    onClick = { backStack.add(ComponentDestination.Badge.BoxBadge as ComponentDestination.Badge as SDGDestination) }
+                )
+            }
 
-                ComponentDestination.Badge.BoxBadge -> NavEntry(key) {
-                    BoxBadgeScreen(
-                        onClick = { backStack.add(ComponentDestination.Avatar as ComponentDestination as SDGDestination) }
-                    )
-                }
-
-                else -> NavEntry(key) {
-                    EmptyScreen(
-                        onClick = { backStack.add(ComponentDestination.Avatar as ComponentDestination as SDGDestination) }
-                    )
-                }
+            entry<ComponentDestination.Badge.BoxBadge> {
+                BoxBadgeScreen(
+                    onClick = { backStack.add(ComponentDestination.Avatar as ComponentDestination as SDGDestination) }
+                )
             }
         }
+//        entryProvider = { key ->
+//            when (key) {
+//                SDGDestination.Home -> NavEntry(key) {
+//                    HomeScreen(
+//                        onClick = { backStack.add(ComponentDestination.Avatar as ComponentDestination as SDGDestination) }
+//                    )
+//                }
+//
+//                ComponentDestination.Avatar -> NavEntry(key) {
+//                    AvatarScreen(
+//                        onClick = { backStack.add(ComponentDestination.Badge.CapsuleBadge as ComponentDestination.Badge as SDGDestination) }
+//                    )
+//                }
+//
+//                ComponentDestination.Badge.CapsuleBadge -> NavEntry(key) {
+//                    CapsuleBadgeScreen(
+//                        onClick = { backStack.add(ComponentDestination.Badge.BoxBadge as ComponentDestination.Badge as SDGDestination) }
+//                    )
+//                }
+//
+//                ComponentDestination.Badge.BoxBadge -> NavEntry(key) {
+//                    BoxBadgeScreen(
+//                        onClick = { backStack.add(ComponentDestination.Avatar as ComponentDestination as SDGDestination) }
+//                    )
+//                }
+//
+//                else -> NavEntry(key) {
+//                    EmptyScreen(
+//                        onClick = { backStack.add(ComponentDestination.Avatar as ComponentDestination as SDGDestination) }
+//                    )
+//                }
+//            }
+//        }
     )
 }
