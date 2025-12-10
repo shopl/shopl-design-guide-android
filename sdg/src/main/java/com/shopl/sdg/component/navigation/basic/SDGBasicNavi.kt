bae6cdbea8b3bc67cc640a64ff35864bc17f3f3a
Scott <thd0427__@naver.com>
2025-12-10 14:09:48 +0900

    불필요 주석 제거

--- sdg/src/main/java/com/shopl/sdg/component/navigation/basic/SDGBasicNavi.kt
+++ sdg/src/main/java/com/shopl/sdg/component/navigation/basic/SDGBasicNavi.kt
@@ -35,8 +35,6 @@ import kotlinx.collections.immutable.PersistentList
  *
  * 화면의 상단에 위치하며, 타이틀과 아이콘으로 조합된 템플릿
  *
- * @param
- *
  * @see <a href="https://www.figma.com/design/qWVshatQ9eqoIn4fdEZqWy/SDG?node-id=19779-9926&t=0IkMpxxx2xWJ4zUm-4">Figma</a>
  */
 @Composable
