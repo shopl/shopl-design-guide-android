96c6f1f4a7915e7cdff75f18c8e7148df39ec31a
Scott <thd0427__@naver.com>
2025-12-10 10:04:24 +0900

    sdg basic navi 파일 추가

---
+++ sdg/src/main/java/com/shopl/sdg/component/navigation/basic/SDGBasicNavi.kt
@@ -0,0 +1,18 @@
+package com.shopl.sdg.component.navigation.basic
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+/**
+ * SDG - Navigation - Basic Navi
+ *
+ * 화면의 상단에 위치하며, 타이틀과 아이콘으로 조합된 템플릿
+ *
+ * @param
+ *
+ * @see <a href="https://www.figma.com/design/qWVshatQ9eqoIn4fdEZqWy/SDG?node-id=19779-9926&t=0IkMpxxx2xWJ4zUm-4">Figma</a>
+ */
+@Composable
+fun SDGBasicNavi(modifier: Modifier = Modifier) {
+
+}
