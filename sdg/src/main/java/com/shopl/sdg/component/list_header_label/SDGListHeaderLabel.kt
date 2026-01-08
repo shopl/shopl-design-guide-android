88d65ec5e205ca194acfe89da7c854dcee1bf860
Scott <thd0427__@naver.com>
2026-01-08 16:03:32 +0900

    list header label 파일 추가

---
+++ sdg/src/main/java/com/shopl/sdg/component/list_header_label/SDGListHeaderLabel.kt
@@ -0,0 +1,18 @@
+package com.shopl.sdg.component.list_header_label
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+/**
+ * SDG -
+ *
+ * description
+ *
+ * @param
+ *
+ * @see <a href="">Figma</a>
+ */
+@Composable
+fun SDGListHeaderLabel(modifier: Modifier = Modifier) {
+
+}
