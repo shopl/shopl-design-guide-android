bfe52766f3e4cc75b70373e058584d40caf17296
Scott <thd0427__@naver.com>
2026-01-08 16:30:42 +0900

    row scope preview 추가

--- sdg/src/main/java/com/shopl/sdg/component/util/list_header_label/SDGListHeaderLabel.kt
+++ sdg/src/main/java/com/shopl/sdg/component/util/list_header_label/SDGListHeaderLabel.kt
@@ -1,10 +1,19 @@
 package com.shopl.sdg.component.util.list_header_label
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import com.shopl.sdg.component.list_header_label.SDGListHeaderLabel
+import com.shopl.sdg_common.foundation.SDGColor
+import com.shopl.sdg_common.foundation.spacing.SDGSpacing.Spacing4
+import com.shopl.sdg_common.ui.components.SDGImage
+import com.shopl.sdg_resource.R
 
 /**
  * [RowScope] [SDGListHeaderLabel]
@@ -28,3 +37,25 @@ fun RowScope.SDGListHeaderLabel(
     }
 }
 
+@Preview(showBackground = true)
+@Composable
+private fun PreviewSDGListHeaderLabel() {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(Spacing4)
+    ) {
+        Row(modifier = Modifier.fillMaxWidth()) {
+            SDGListHeaderLabel(
+                weight = 1f,
+                title = "Weighted 1",
+                count = "1"
+            )
+            
+            SDGImage(
+                resId = R.drawable.ic_alignup,
+                color = SDGColor.Neutral700,
+            )
+        }
+    }
+}
