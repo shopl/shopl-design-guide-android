7c1a8888c34409525f412cfc52715290a9b21849
Scott <thd0427__@naver.com>
2026-01-08 16:07:42 +0900

    list header label 구현

--- sdg/src/main/java/com/shopl/sdg/component/list_header_label/SDGListHeaderLabel.kt
+++ sdg/src/main/java/com/shopl/sdg/component/list_header_label/SDGListHeaderLabel.kt
@@ -1,7 +1,19 @@
 package com.shopl.sdg.component.list_header_label
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.shopl.sdg_common.ext.clickable
+import com.shopl.sdg_common.foundation.SDGColor
+import com.shopl.sdg_common.foundation.spacing.SDGSpacing.Spacing3
+import com.shopl.sdg_common.foundation.typography.SDGTypography
+import com.shopl.sdg_common.ui.components.SDGImage
+import com.shopl.sdg_common.ui.components.SDGText
+import com.shopl.sdg_resource.R
 
 /**
  * SDG -
@@ -13,6 +25,35 @@ import androidx.compose.ui.Modifier
  * @see <a href="">Figma</a>
  */
 @Composable
-fun SDGListHeaderLabel(modifier: Modifier = Modifier) {
+fun SDGListHeaderLabel(
+    title: String,
+    count: String?,
+    onIconClick: (() -> Unit)? = null,
+) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.spacedBy(Spacing3)
+    ) {
+        SDGText(
+            text = title,
+            textColor = SDGColor.Neutral700,
+            typography = SDGTypography.Body1SB
+        )
 
+        if (count != null) {
+            SDGText(
+                text = count,
+                textColor = SDGColor.Neutral700,
+                typography = SDGTypography.Body1SB
+            )
+        }
+
+        SDGImage(
+            modifier = Modifier
+                .size(20.dp)
+                .clickable { onIconClick?.invoke() },
+            resId = R.drawable.ic_common_dropdown,
+            color = SDGColor.Neutral700
+        )
+    }
 }
