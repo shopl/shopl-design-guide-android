d203c382cfe5b62e8b6327f90ec6fd4a0864c514
san-gun <sangun.kim@shoplworks.com>
2026-01-09 13:12:52 +0900

    [SH-16667] [Android] Template > Radio Label 가이드 부분 수정 및 업데이트 (#236)

---
+++ sdg/src/main/java/com/shopl/sdg/template/radio_label/SDGRadioLabelColor.kt
@@ -0,0 +1,9 @@
+package com.shopl.sdg.template.radio_label
+
+import androidx.compose.ui.graphics.Color
+import com.shopl.sdg_common.foundation.SDGColor
+
+enum class SDGRadioLabelColor(val color: Color) {
+    BASIC(color = SDGColor.Neutral700),
+    COLOR(color = SDGColor.Primary300),
+}
