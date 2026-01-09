d0566fe4a5474a13cd85c231968bcebc5646546e
Sangun <sangun.kim@shoplworks.com>
2026-01-09 13:03:56 +0900

    SDGRadio isEnabled제거 및 프리뷰 파라미터 수정

--- sdg/src/main/java/com/shopl/sdg/component/radio/SDGRadioColor.kt
+++ sdg/src/main/java/com/shopl/sdg/component/radio/SDGRadioColor.kt
@@ -5,5 +5,5 @@ import com.shopl.sdg_common.foundation.SDGColor
 
 enum class SDGRadioColor(val color: Color) {
     BASIC(color = SDGColor.Primary300),
-    SPECIAL(color = SDGColor.Neutral700)
+    SPECIAL(color = SDGColor.Neutral700),
 }
