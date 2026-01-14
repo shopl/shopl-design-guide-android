f8af04dce83ac02f331fd879875e69c59ae79a3e
willy <willy.jung@shoplworks.com>
2026-01-14 11:30:54 +0900

    구조 변경

--- app/src/main/java/com/shopl/sdg/scene/FoundationScene.kt
+++ app/src/main/java/com/shopl/sdg/scene/FoundationScene.kt
@@ -1,4 +1,4 @@
-package com.shopl.sdg.navigation
+package com.shopl.sdg.scene
 
 import androidx.compose.runtime.Stable
 
@@ -8,31 +8,31 @@ import androidx.compose.runtime.Stable
  * @see <a href="https://www.figma.com/design/qWVshatQ9eqoIn4fdEZqWy/SDG?node-id=4739-18081&m=dev">Figma</a>
  */
 @Stable
-sealed class FoundationDestination(
+sealed class FoundationScene(
     override val displayLabel: String,
     override val implemented: Boolean,
-) : SDGDestination(displayLabel, implemented) {
-    data object Color : FoundationDestination(
+) : SDGScene(displayLabel, implemented) {
+    data object Color : FoundationScene(
         displayLabel = "Color",
         implemented = false
     )
 
-    data object CornerRadius : FoundationDestination(
+    data object CornerRadius : FoundationScene(
         displayLabel = "Corner Radius",
         implemented = false
     )
 
-    data object Iconography : FoundationDestination(
+    data object Iconography : FoundationScene(
         displayLabel = "Iconography",
         implemented = false
     )
 
-    data object Spacing : FoundationDestination(
+    data object Spacing : FoundationScene(
         displayLabel = "Spacing",
         implemented = false
     )
 
-    data object Typograph : FoundationDestination(
+    data object Typograph : FoundationScene(
         displayLabel = "Typograph",
         implemented = false
     )
