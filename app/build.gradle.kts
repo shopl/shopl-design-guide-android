0615895fac4e5ae4072479e51a9851d0fb54f076
willy <willy.jung@shoplworks.com>
2026-01-14 13:31:06 +0900

    navigation 처리, status bar 색상 제어 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -63,4 +63,6 @@ dependencies {
 
     implementation(libs.androidx.navigation3.ui)
     implementation(libs.androidx.navigation3.runtime)
+
+    implementation(libs.accompanist.systemuicontroller)
 }
--- app/src/main/java/com/shopl/sdg/navigation/NavEntryProvider.kt
+++ app/src/main/java/com/shopl/sdg/navigation/NavEntryProvider.kt
@@ -1,30 +1,58 @@
 package com.shopl.sdg.navigation
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
 import androidx.navigation3.runtime.NavEntry
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import com.shopl.sdg.scene.ComponentScene
 import com.shopl.sdg.scene.SDGScene
 import com.shopl.sdg.ui.screen.OverviewScreen
 import com.shopl.sdg.ui.screen.component.AvatarScreen
 import com.shopl.sdg.ui.screen.component.button.BoxButtonScreen
+import com.shopl.sdg_common.foundation.SDGColor
 
-internal fun provideNavEntry(destination: SDGScene): NavEntry<Any> {
+internal fun provideNavEntry(
+    destination: SDGScene,
+    moveToScene: (SDGScene) -> Unit,
+): NavEntry<Any> {
     return NavEntry(destination) {
         when (destination) {
-            is SDGScene.Overview -> OverviewScreenRoute()
-            is ComponentScene -> ComponentScreenRoute(destination)
+            is SDGScene.Overview -> OverviewScreenRoute(
+                moveToScene = moveToScene
+            )
+            is ComponentScene -> ComponentScreenRoute(
+                destination = destination
+            )
             else -> {}
         }
     }
 }
 
 @Composable
-private fun OverviewScreenRoute() {
-    OverviewScreen()
+private fun OverviewScreenRoute(
+    moveToScene: (SDGScene) -> Unit,
+) {
+    val systemUiController = rememberSystemUiController()
+    SideEffect {
+        systemUiController.setStatusBarColor(
+            color = SDGColor.Transparent,
+            darkIcons = false
+        )
+    }
+    OverviewScreen(
+        moveToScene = moveToScene
+    )
 }
 
 @Composable
 private fun ComponentScreenRoute(destination: ComponentScene) {
+    val systemUiController = rememberSystemUiController()
+    SideEffect {
+        systemUiController.setStatusBarColor(
+            color = SDGColor.Transparent,
+            darkIcons = true
+        )
+    }
     when (destination) {
         is ComponentScene.Avatar -> {
             AvatarScreen()
