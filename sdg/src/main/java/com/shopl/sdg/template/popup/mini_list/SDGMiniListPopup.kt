d2ce938585ae0f0f3efd5bb82971834293dd3565
Scott <thd0427__@naver.com>
2026-02-11 09:52:00 +0900

    주석 추가

--- sdg/src/main/java/com/shopl/sdg/template/popup/mini_list/SDGMiniListPopup.kt
+++ sdg/src/main/java/com/shopl/sdg/template/popup/mini_list/SDGMiniListPopup.kt
@@ -1,7 +1,16 @@
 package com.shopl.sdg.template.popup.mini_list
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
 /**
+ * SDG - Popup - Mini List Popup
  *
+ * 앱 콘텐츠 앞 특정 위치에 표시되며, 단순 동작 선택을 위한 콘텐츠를 포함하는 모달형 템플릿
+ *
+ * @see <a href="https://www.figma.com/design/qWVshatQ9eqoIn4fdEZqWy/SDG?node-id=19220-4063&m=dev">Figma</a>
  */
-class SDGMiniListPopup {
+@Composable
+fun SDGMiniListPopup(modifier: Modifier = Modifier) {
+    
 }
