0a37a774959df599e72ee1724958b225af3b7637
Scott <thd0427__@naver.com>
2026-02-11 09:52:13 +0900

    버전 추가 주석

--- sdg/src/main/java/com/shopl/sdg/template/popup/mini_list/SDGMiniListPopup.kt
+++ sdg/src/main/java/com/shopl/sdg/template/popup/mini_list/SDGMiniListPopup.kt
@@ -4,7 +4,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 
 /**
- * SDG - Popup - Mini List Popup
+ * SDG - Popup - Mini List Popup [2.0.0]
  *
  * 앱 콘텐츠 앞 특정 위치에 표시되며, 단순 동작 선택을 위한 콘텐츠를 포함하는 모달형 템플릿
  *
