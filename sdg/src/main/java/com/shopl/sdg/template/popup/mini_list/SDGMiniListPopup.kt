aa200d6d3f5830d2558cca93ed64c5364c6ada47
Scott <thd0427__@naver.com>
2026-02-19 15:16:51 +0900

    고정 width 추가

--- sdg/src/main/java/com/shopl/sdg/template/popup/mini_list/SDGMiniListPopup.kt
+++ sdg/src/main/java/com/shopl/sdg/template/popup/mini_list/SDGMiniListPopup.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -33,7 +34,9 @@ fun SDGMiniListPopup(
     items: ImmutableList<SDGMiniListPopupBodyItemText>,
     onClick: (itemText: SDGMiniListPopupBodyItemText) -> Unit
 ) {
-    Column {
+    Column(
+        modifier = Modifier.width(200.dp)
+    ) {
         items.forEachIndexed { index, item ->
             SDGMiniListPopupBodyItem(
                 itemText = item,
