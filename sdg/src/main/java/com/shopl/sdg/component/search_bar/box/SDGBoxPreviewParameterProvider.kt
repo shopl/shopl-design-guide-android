e4f69f7cc01e49b5cff4854b584bd80ae1170c3d
willy <willy.jung@shoplworks.com>
2026-02-25 15:14:20 +0900

    [SDG] Component > SearchBar > Box Preview 수정

--- sdg/src/main/java/com/shopl/sdg/component/search_bar/box/SDGBoxPreviewParameterProvider.kt
+++ sdg/src/main/java/com/shopl/sdg/component/search_bar/box/SDGBoxPreviewParameterProvider.kt
@@ -4,21 +4,51 @@ import androidx.compose.ui.tooling.preview.PreviewParameterProvider
 
 internal class SDGBoxPreviewParameterProvider : PreviewParameterProvider<SDGBoxPreviewParameter> {
     override val values: Sequence<SDGBoxPreviewParameter> = sequenceOf(
-        SDGBoxPreviewParameter(SDGBoxSearchType.Solid(), "입력중", true),
-        SDGBoxPreviewParameter(
-            SDGBoxSearchType.Solid(
-                backgroundColor = SDGBoxSearchType.Solid.SolidColor.Neutral50,
-            ),
-            "",
-            true,
+        Solid_기본(),
+        Solid_입력중(),
+        Solid_비활성(),
+        Solid_입력중_비활성(),
+        Solid_NEUTRAL50(),
+        Line_기본()
+    )
+
+    private fun Solid_기본() = SDGBoxPreviewParameter(
+        type = SDGBoxSearchType.Solid(),
+        input = ""
+    )
+
+    private fun Solid_입력중() = SDGBoxPreviewParameter(
+        type = SDGBoxSearchType.Solid(),
+        input = "입력중"
+    )
+
+    private fun Solid_비활성() = SDGBoxPreviewParameter(
+        type = SDGBoxSearchType.Solid(),
+        input = "",
+        enabled = false
+    )
+
+    private fun Solid_입력중_비활성() = SDGBoxPreviewParameter(
+        type = SDGBoxSearchType.Solid(),
+        input = "입력중 비활성 됨",
+        enabled = false
+    )
+
+    private fun Solid_NEUTRAL50() = SDGBoxPreviewParameter(
+        type = SDGBoxSearchType.Solid(
+            backgroundColor = SDGBoxSearchType.Solid.SolidColor.Neutral50,
         ),
-        SDGBoxPreviewParameter(SDGBoxSearchType.Line, "", true),
-        SDGBoxPreviewParameter(SDGBoxSearchType.Line, "Disabled", false),
+        input = ""
+    )
+
+    private fun Line_기본() = SDGBoxPreviewParameter(
+        type = SDGBoxSearchType.Line,
+        input = ""
     )
 }
 
 internal data class SDGBoxPreviewParameter(
     val type: SDGBoxSearchType,
     val input: String,
-    val enabled: Boolean,
+    val enabled: Boolean = true,
 )
