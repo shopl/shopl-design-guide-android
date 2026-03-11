d3614e667c549336e36a00cd4bee77ac30957091
Scott <thd0427__@naver.com>
2026-03-11 16:03:07 +0900

    ListHeaderLabel Preview 메서드명 한글화

--- sdg/src/main/java/com/shopl/sdg/component/list_header_label/preview/SDGListHeaderLabelPreviewParameterProvider.kt
+++ sdg/src/main/java/com/shopl/sdg/component/list_header_label/preview/SDGListHeaderLabelPreviewParameterProvider.kt
@@ -6,38 +6,38 @@ internal class SDGListHeaderLabelPreviewParameterProvider :
     PreviewParameterProvider<SDGListHeaderLabelPreviewParams> {
 
     override val values: Sequence<SDGListHeaderLabelPreviewParams> = sequenceOf(
-        Title_Only(),
-        Title_With_Count(),
-        Title_With_Icon(),
-        Title_With_Count_And_Icon(),
-        Long_Title_With_Count_And_Icon(),
+        타이틀만(),
+        타이틀_카운트(),
+        타이틀_드롭다운아이콘(),
+        타이틀_카운트_드롭다운아이콘(),
+        긴타이틀_카운트_드롭다운아이콘(),
     )
 
-    private fun Title_Only() = SDGListHeaderLabelPreviewParams(
+    private fun 타이틀만() = SDGListHeaderLabelPreviewParams(
         title = "리스트 타이틀",
         count = null,
         dropdownIcon = false
     )
 
-    private fun Title_With_Count() = SDGListHeaderLabelPreviewParams(
+    private fun 타이틀_카운트() = SDGListHeaderLabelPreviewParams(
         title = "리스트 타이틀",
         count = "10",
         dropdownIcon = false
     )
 
-    private fun Title_With_Icon() = SDGListHeaderLabelPreviewParams(
+    private fun 타이틀_드롭다운아이콘() = SDGListHeaderLabelPreviewParams(
         title = "리스트 타이틀",
         count = null,
         dropdownIcon = true
     )
 
-    private fun Title_With_Count_And_Icon() = SDGListHeaderLabelPreviewParams(
+    private fun 타이틀_카운트_드롭다운아이콘() = SDGListHeaderLabelPreviewParams(
         title = "리스트 타이틀",
         count = "99+",
         dropdownIcon = true
     )
 
-    private fun Long_Title_With_Count_And_Icon() = SDGListHeaderLabelPreviewParams(
+    private fun 긴타이틀_카운트_드롭다운아이콘() = SDGListHeaderLabelPreviewParams(
         title = "리스트 타이틀이 매우 길어서 화면을 넘어가는 경우에 대한 테스트입니다.",
         count = "99+",
         dropdownIcon = true
