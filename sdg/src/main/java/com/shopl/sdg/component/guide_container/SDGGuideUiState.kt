b0f64a1564a2095b2f757b0fd636c1e08799ca1b
Sangun <sangun.kim@shoplworks.com>
2026-03-12 14:08:06 +0900

    @Stable 마커 추가

--- sdg/src/main/java/com/shopl/sdg/component/guide_container/SDGGuideUiState.kt
+++ sdg/src/main/java/com/shopl/sdg/component/guide_container/SDGGuideUiState.kt
@@ -1,8 +1,10 @@
 package com.shopl.sdg.component.guide_container
 
+import androidx.compose.runtime.Stable
 import androidx.compose.ui.graphics.Color
 import com.shopl.sdg_common.foundation.SDGColor
 
+@Stable
 sealed interface SDGGuideUiState {
     data class Show(
         val message: String,
