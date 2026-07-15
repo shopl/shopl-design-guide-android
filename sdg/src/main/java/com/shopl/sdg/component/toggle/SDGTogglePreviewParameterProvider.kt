f2554c80517294f92a34d02c60320be151934917
willy <willy.jung@shoplworks.com>
2026-07-15 13:40:24 +0900

    PreviewParameterProvider 추가

---
+++ sdg/src/main/java/com/shopl/sdg/component/toggle/SDGTogglePreviewParameterProvider.kt
@@ -0,0 +1,115 @@
+package com.shopl.sdg.component.toggle
+
+import androidx.compose.ui.tooling.preview.PreviewParameterProvider
+import com.shopl.sdg.component.toggle.spec.SDGToggleSpec
+import com.shopl.sdg.component.toggle.style.SDGToggleStyle
+
+internal class SDGCapsulePreviewParameterProvider :
+    PreviewParameterProvider<SDGTogglePreviewParameter> {
+    override val values: Sequence<SDGTogglePreviewParameter> = sequenceOf(
+        off_PRIMARY_MEDIUM(),
+        on_PRIMARY_MEDIUM(),
+        disable_PRIMARY_MEDIUM(),
+        off_NEUTRAL_MEDIUM(),
+        on_NEUTRAL_MEDIUM(),
+        disable_NEUTRAL_MEDIUM(),
+        off_PRIMARY_SMALL(),
+        on_PRIMARY_SMALL(),
+        disable_PRIMARY_SMALL(),
+        off_NEUTRAL_SMALL(),
+        on_NEUTRAL_SMALL(),
+        disable_NEUTRAL_SMALL(),
+    )
+
+    private fun off_PRIMARY_MEDIUM() = SDGTogglePreviewParameter(
+        isOn = false,
+        isEnabled = true,
+        style = SDGToggleStyle.PRIMARY,
+        spec = SDGToggleSpec.MEDIUM
+    )
+
+    private fun on_PRIMARY_MEDIUM() = SDGTogglePreviewParameter(
+        isOn = true,
+        isEnabled = true,
+        style = SDGToggleStyle.PRIMARY,
+        spec = SDGToggleSpec.MEDIUM
+    )
+
+    private fun disable_PRIMARY_MEDIUM() = SDGTogglePreviewParameter(
+        isOn = false,
+        isEnabled = false,
+        style = SDGToggleStyle.PRIMARY,
+        spec = SDGToggleSpec.MEDIUM
+    )
+
+    private fun off_NEUTRAL_MEDIUM() = SDGTogglePreviewParameter(
+        isOn = false,
+        isEnabled = true,
+        style = SDGToggleStyle.NEUTRAL,
+        spec = SDGToggleSpec.MEDIUM
+    )
+
+    private fun on_NEUTRAL_MEDIUM() = SDGTogglePreviewParameter(
+        isOn = true,
+        isEnabled = true,
+        style = SDGToggleStyle.NEUTRAL,
+        spec = SDGToggleSpec.MEDIUM
+    )
+
+    private fun disable_NEUTRAL_MEDIUM() = SDGTogglePreviewParameter(
+        isOn = false,
+        isEnabled = false,
+        style = SDGToggleStyle.NEUTRAL,
+        spec = SDGToggleSpec.MEDIUM
+    )
+
+    private fun off_PRIMARY_SMALL() = SDGTogglePreviewParameter(
+        isOn = false,
+        isEnabled = true,
+        style = SDGToggleStyle.PRIMARY,
+        spec = SDGToggleSpec.SMALL
+    )
+
+    private fun on_PRIMARY_SMALL() = SDGTogglePreviewParameter(
+        isOn = true,
+        isEnabled = true,
+        style = SDGToggleStyle.PRIMARY,
+        spec = SDGToggleSpec.SMALL
+    )
+
+    private fun disable_PRIMARY_SMALL() = SDGTogglePreviewParameter(
+        isOn = false,
+        isEnabled = false,
+        style = SDGToggleStyle.PRIMARY,
+        spec = SDGToggleSpec.SMALL
+    )
+
+    private fun off_NEUTRAL_SMALL() = SDGTogglePreviewParameter(
+        isOn = false,
+        isEnabled = true,
+        style = SDGToggleStyle.NEUTRAL,
+        spec = SDGToggleSpec.SMALL
+    )
+
+    private fun on_NEUTRAL_SMALL() = SDGTogglePreviewParameter(
+        isOn = true,
+        isEnabled = true,
+        style = SDGToggleStyle.NEUTRAL,
+        spec = SDGToggleSpec.SMALL
+    )
+
+    private fun disable_NEUTRAL_SMALL() = SDGTogglePreviewParameter(
+        isOn = false,
+        isEnabled = false,
+        style = SDGToggleStyle.NEUTRAL,
+        spec = SDGToggleSpec.SMALL
+    )
+
+}
+
+internal data class SDGTogglePreviewParameter(
+    val isOn: Boolean,
+    val isEnabled: Boolean,
+    val style: SDGToggleStyle,
+    val spec: SDGToggleSpec,
+)
