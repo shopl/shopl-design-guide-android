21749be134b88afc631346cc356a17bcf2f3c140
willy <willy.jung@shoplworks.com>
2026-07-15 15:36:45 +0900

    [SH-21996] Search bar > Capsule Search > Backgound Color 속성 추가

---
+++ sdg/src/main/java/com/shopl/sdg/component/search_bar/capsule/SDGCapsuleSearchBackgroundColor.kt
@@ -0,0 +1,11 @@
+package com.shopl.sdg.component.search_bar.capsule
+
+import androidx.compose.ui.graphics.Color
+import com.shopl.sdg_common.foundation.SDGColor
+
+enum class SDGCapsuleSearchBackgroundColor(
+    val color: Color
+) {
+    WHITE(color = SDGColor.Neutral0),
+    LIGHT_GRAY(color = SDGColor.Neutral50),
+}
