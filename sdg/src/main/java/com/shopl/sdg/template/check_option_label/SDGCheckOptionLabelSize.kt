5c8a7f3dd2744ef403b92717b5ec0ba5ad6078bc
Scott <thd0427__@naver.com>
2026-09-07 09:16:04 +0900

    Size 프로퍼티 추가

--- sdg/src/main/java/com/shopl/sdg/template/check_option_label/SDGCheckOptionLabelSize.kt
+++ sdg/src/main/java/com/shopl/sdg/template/check_option_label/SDGCheckOptionLabelSize.kt
@@ -1,8 +1,25 @@
 package com.shopl.sdg.template.check_option_label
 
+import androidx.compose.ui.unit.Dp
+import com.shopl.sdg_common.foundation.spacing.SDGSpacing.Spacing1
+import com.shopl.sdg_common.foundation.spacing.SDGSpacing.Spacing2
+import com.shopl.sdg_common.foundation.spacing.SDGSpacing.Spacing6
+import com.shopl.sdg_common.foundation.spacing.SDGSpacing.Spacing8
 import com.shopl.sdg_common.foundation.typography.SDGTypography
 
-enum class SDGCheckOptionLabelSize(val typography: SDGTypography) {
-    SMALL(typography = SDGTypography.Body2R),
-    MEDIUM(typography = SDGTypography.Body1R)
+enum class SDGCheckOptionLabelSize(
+    val typography: SDGTypography,
+    internal val gap: Dp,
+    internal val checkOptionVerticalPadding: Dp,
+) {
+    SMALL(
+        typography = SDGTypography.Body2R,
+        gap = Spacing6,
+        checkOptionVerticalPadding = Spacing1,
+    ),
+    MEDIUM(
+        typography = SDGTypography.Body1R,
+        gap = Spacing8,
+        checkOptionVerticalPadding = Spacing2,
+    ),
 }
