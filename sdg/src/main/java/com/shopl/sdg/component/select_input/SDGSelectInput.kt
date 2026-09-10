5e85ff95e0fcc220fbfe8bc0849017d1c23dac25
Scott <thd0427__@naver.com>
2026-09-10 11:19:52 +0900

    min height 적용

--- sdg/src/main/java/com/shopl/sdg/component/select_input/SDGSelectInput.kt
+++ sdg/src/main/java/com/shopl/sdg/component/select_input/SDGSelectInput.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
@@ -143,6 +144,7 @@ private fun SelectInputField(
                     Modifier
                 },
             )
+            .heightIn(min = SDGSelectInputDefaultHeight)
             .padding(
                 horizontal = SDGSpacing.Spacing12,
                 vertical = SDGSpacing.Spacing5,
